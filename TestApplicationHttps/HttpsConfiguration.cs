77fb1144670fb54be99ed2bd3f933a1b199e2903
Me <Me@example.com>
2020-12-06 22:37:51 +0100

    Added https cert

---
+++ TestApplicationHttps/HttpsConfiguration.cs
@@ -0,0 +1,157 @@
+﻿
+using Microsoft.AspNetCore.Hosting; // for UseHttps
+
+
+namespace TestApplicationHttps.Configuration.Kestrel 
+{
+
+
+    public static class Https
+    {
+
+
+        public static void HttpsDefaults(Microsoft.AspNetCore.Server.Kestrel.Https.HttpsConnectionAdapterOptions listenOptions)
+        {
+            listenOptions.OnAuthenticate =
+                delegate (Microsoft.AspNetCore.Connections.ConnectionContext connectionContext, System.Net.Security.SslServerAuthenticationOptions sslOptions)
+                {
+                    sslOptions.CipherSuitesPolicy = new System.Net.Security.CipherSuitesPolicy(
+                           new System.Net.Security.TlsCipherSuite[]
+                           {
+                                System.Net.Security.TlsCipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                                System.Net.Security.TlsCipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+                                System.Net.Security.TlsCipherSuite.TLS_CHACHA20_POLY1305_SHA256,
+                               // ...
+                           });
+                }
+            ; // End OnAuthenticate 
+
+        }
+
+        public static void CertificateFileChanged(
+              System.Collections.Concurrent.ConcurrentDictionary<string, System.Security.Cryptography.X509Certificates.X509Certificate2> certs
+            , object sender
+            , System.IO.FileSystemEventArgs e
+            )
+        {
+            System.Console.WriteLine(e.FullPath.ToString() + " is changed!");
+            // TODO: Swap certificate...
+            // certs["localhost"] = localhostCert;
+        }
+
+
+        public static System.Security.Cryptography.X509Certificates.X509Certificate2 ServerCertificateSelector(
+              System.Collections.Concurrent.ConcurrentDictionary<string, System.Security.Cryptography.X509Certificates.X509Certificate2> certs
+            , Microsoft.AspNetCore.Connections.ConnectionContext connectionContext
+            , string name)
+        {
+            System.Security.Cryptography.X509Certificates.X509Certificate2 cert;
+
+            if (certs.Count > 0)
+            {
+                return certs.GetEnumerator().Current.Value;
+            }
+
+
+            /*
+            if (name != null && certs.TryGetValue(name, out cert))
+            {
+                return cert;
+            }
+            */
+
+            throw new System.IO.InvalidDataException("No certificate for name \"" + name + "\".");
+        } // End Function ServerCertificateSelector 
+
+
+        public static void ListenAnyIP(
+              Microsoft.AspNetCore.Server.Kestrel.Core.ListenOptions listenOptions
+            , System.IO.FileSystemWatcher watcher 
+            )
+        {
+            System.Collections.Concurrent.ConcurrentDictionary<string, System.Security.Cryptography.X509Certificates.X509Certificate2> certs =
+                                                new System.Collections.Concurrent.ConcurrentDictionary<string, System.Security.Cryptography.X509Certificates.X509Certificate2>(
+                                                    System.StringComparer.OrdinalIgnoreCase
+                                            );
+
+            // watcher.Filters.Add("localhost.yml");
+            // watcher.Filters.Add("example.com.yaml");
+            // watcher.Filters.Add("sub.example.com.yaml");
+
+            System.IO.FileSystemEventHandler onChange = delegate (object sender, System.IO.FileSystemEventArgs e)
+            {
+                CertificateFileChanged(certs, sender, e);
+            };
+
+
+            watcher.Changed += new System.IO.FileSystemEventHandler(onChange);
+            watcher.Created += new System.IO.FileSystemEventHandler(onChange);
+            watcher.Deleted += new System.IO.FileSystemEventHandler(onChange);
+            // watcher.Renamed += new System.IO.RenamedEventHandler(OnRenamed);
+
+            watcher.EnableRaisingEvents = true;
+
+
+            listenOptions.UseHttps(
+                delegate (Microsoft.AspNetCore.Server.Kestrel.Https.HttpsConnectionAdapterOptions httpsOptions)
+                {
+                    UseHttps(certs, httpsOptions);
+
+                    httpsOptions.ServerCertificateSelector =
+                        delegate (Microsoft.AspNetCore.Connections.ConnectionContext connectionContext, string name)
+                        {
+                            return ServerCertificateSelector(certs, connectionContext, name);
+                        };
+
+                }
+            ); // End ListenOptions.UseHttps
+
+        } // End Sub ListenAnyIP 
+
+        public static void UseHttps(
+              System.Collections.Concurrent.ConcurrentDictionary<string, System.Security.Cryptography.X509Certificates.X509Certificate2> certs
+            , Microsoft.AspNetCore.Server.Kestrel.Https.HttpsConnectionAdapterOptions httpsOptions)
+        {
+            /*
+            System.Security.Cryptography.X509Certificates.X509Certificate2 localhostCert = Microsoft.AspNetCore.Server.Kestrel.Https.CertificateLoader.LoadFromStoreCert(
+                "localhost", "My", System.Security.Cryptography.X509Certificates.StoreLocation.CurrentUser,
+                allowInvalid: true);
+
+            System.Security.Cryptography.X509Certificates.X509Certificate2 exampleCert = Microsoft.AspNetCore.Server.Kestrel.Https.CertificateLoader.LoadFromStoreCert(
+                "example.com", "My", System.Security.Cryptography.X509Certificates.StoreLocation.CurrentUser,
+                allowInvalid: true);
+
+            System.Security.Cryptography.X509Certificates.X509Certificate2 subExampleCert = Microsoft.AspNetCore.Server.Kestrel.Https.CertificateLoader.LoadFromStoreCert(
+                "sub.example.com", "My", System.Security.Cryptography.X509Certificates.StoreLocation.CurrentUser,
+                allowInvalid: true);
+
+            certs["localhost"] = localhostCert;
+            certs["example.com"] = exampleCert;
+            certs["sub.example.com"] = subExampleCert;
+            */
+
+            string cert = SecretManager.GetSecret<string>("ssl_cert");
+            string key = SecretManager.GetSecret<string>("ssl_key");
+
+            System.ReadOnlySpan<char> certSpan = System.MemoryExtensions.AsSpan(cert);
+            System.ReadOnlySpan<char> keySpan = System.MemoryExtensions.AsSpan(key);
+
+
+            System.Security.Cryptography.X509Certificates.X509Certificate2 certSslLoaded = System.Security.Cryptography.X509Certificates.X509Certificate2.CreateFromPem(certSpan, keySpan);
+
+
+            certs["localhost"] = certSslLoaded;
+            httpsOptions.ServerCertificateSelector =
+                delegate (Microsoft.AspNetCore.Connections.ConnectionContext connectionContext, string name)
+                {
+                    return ServerCertificateSelector(certs, connectionContext, name);
+                }
+            ;
+
+        } // End Sub UseHttps 
+
+
+    } // End Class Https 
+
+
+} // End Namespace TestApplicationHttps.Configuration.Kestrel 
--- TestApplicationHttps/Program.cs
+++ TestApplicationHttps/Program.cs
@@ -1,30 +1,112 @@
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-// 1. add  <AspNetCoreHostingModel>InProcess</AspNetCoreHostingModel> to project file
-
 
 
 namespace TestApplicationHttps
 {
+
+
+    // 1. add  <AspNetCoreHostingModel>InProcess</AspNetCoreHostingModel> to project file 
     public class Program
     {
+
+
+        // https://github.com/Tondas/LetsEncrypt
+        // https://blogs.akamai.com/2018/10/best-practices-for-ultra-low-latency-streaming-using-chunked-encoded-and-chunk-transferred-cmaf.html
+        // https://www.monitis.com/blog/how-to-log-to-postgresql-with-syslog-ng/
+        // https://github.com/sshnet/SSH.NET
+        // https://labs.rebex.net/syslog
+        // https://github.com/moovspace/HotSsl
+        // https://github.com/jchristn/WatsonSyslogServer
+        // https://github.com/dotnet/aspnetcore/discussions/28238#discussioncomment-142844
+        // https://learn.akamai.com/en-us/webhelp/media-services-live/media-services-live-encoder-compatibility-testing-and-qualification-guide-v4.0/GUID-A7D10A31-F4BC-49DD-92B2-8A5BA409BAFE.html#:~:text=The%20transmission%20ends%20when%20a%20zero%2Dlength%20chunk%20is%20received.&text=The%20Content%2DLength%20header%20is,regular%20chunk%20with%20zero%20length.
+        // https://www.drunkcode.net/en/posts/2020/6/10/parsing-with-ReadOnlySpan-and-first-try-of-OBJ-reader
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            using (System.IO.FileSystemWatcher watcher = new System.IO.FileSystemWatcher())
+            {
+                // listenOptions.UseHttps("testCert.pfx", "testPassword");                                
+                watcher.NotifyFilter = System.IO.NotifyFilters.Size
+                    | System.IO.NotifyFilters.LastWrite
+                    | System.IO.NotifyFilters.CreationTime
+                    | System.IO.NotifyFilters.FileName // Needed if text-file is changed with Visual Studio ...
+                ;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+                CreateHostBuilder(args, watcher).Build().Run();
+            } // End Using watcher 
+
+        } // End Sub Main 
+
+
+
+        // https://github.com/dotnet/aspnetcore/discussions/28238
+        // https://github.com/aspnet/KestrelHttpServer/issues/2103
+        // https://ayende.com/blog/181281-A/building-a-lets-encrypt-acme-v2-client
+        // https://weblog.west-wind.com/posts/2016/feb/22/using-lets-encrypt-with-iis-on-windows
+        // https://medium.com/@MaartenSikkema/automatically-request-and-use-lets-encrypt-certificates-in-dotnet-core-9d0d152a59b5
+        // https://github.com/dotnet/aspnetcore/issues/1190
+        // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel?view=aspnetcore-5.0#code-try-30
+        public static IHostBuilder CreateHostBuilder(string[] args, System.IO.FileSystemWatcher watcher)
+        {
+            // Microsoft.AspNetCore.Server.IIS
+
+            return Host
+                .CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(
+                    delegate (IWebHostBuilder webBuilder)
+                    {
+
+#if true 
+
+                        // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel?view=aspnetcore-5.0#code-try-30
+                        webBuilder.ConfigureKestrel(
+                            delegate (
+                                 WebHostBuilderContext builderContext
+                                , Microsoft.AspNetCore.Server.Kestrel.Core.KestrelServerOptions serverOptions)
+                            {
+                                // https://github.com/dotnet/aspnetcore/pull/24286
+                                // From config-file with reload on change 
+                                // serverOptions.Configure(builderContext.Configuration.GetSection("Kestrel"), reloadOnChange: false);
+
+                                // On Linux, CipherSuitesPolicy can be used to filter TLS handshakes on a per-connection basis:
+                                serverOptions.ConfigureHttpsDefaults(Configuration.Kestrel.Https.HttpsDefaults); // End ConfigureHttpsDefaults 
+
+                                // serverOptions.Listen(System.Net.IPAddress.Loopback, 5001,
+                                serverOptions.ListenAnyIP(5005,
+                                    delegate (Microsoft.AspNetCore.Server.Kestrel.Core.ListenOptions listenOptions)
+                                    {
+                                        Configuration.Kestrel.Https.ListenAnyIP(listenOptions, watcher);
+                                    }
+                                ); // End ListenAnyIp 
+
+                            }
+                        ); // End ConfigureKestrel 
+#endif
+
+                        // https://developers.redhat.com/blog/2018/07/24/improv-net-core-kestrel-performance-linux/
+                        // webBuilder.UseLinuxTransport();
+
+                        /*
+                        if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
+                        {
+                            webBuilder.UseIISIntegration();
+                        }
+                        else webBuilder.UseKestrel();
+                        */
+
+                        webBuilder.UseStartup<Startup>()
+                        // .UseApplicationInsights()
+                        ;
+
+                    }); // End ConfigureWebHostDefaults 
+
+        } // End Function CreateHostBuilder 
+
+
+    } // End Class Program 
+
+
+} // End Namespace TestApplicationHttps 
