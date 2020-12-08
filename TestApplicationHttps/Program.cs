6d5a3bd14d6ee81bda2034cab332b8388dcfacf2
ststeiger <quandary82@hailmail.net>
2020-12-08 15:41:49 +0100

    Added ListenOptions

--- TestApplicationHttps/Program.cs
+++ TestApplicationHttps/Program.cs
@@ -23,23 +23,70 @@ namespace TestApplicationHttps
         public string AesIV { get; set; }
 
 
-        public static System.Security.Cryptography.X509Certificates.X509Certificate2 GetCertificate()
+        public System.Security.Cryptography.X509Certificates.X509Certificate2 GetCertificate()
         {
-            return null;
-        }
-    }
-    
-    
+            if (System.IO.File.Exists(this.CertificateFile) && System.IO.File.Exists(this.CertificateKey))
+            {
+                return System.Security.Cryptography.X509Certificates.X509Certificate2.CreateFromPemFile(this.CertificateFile, this.CertificateKey);
+            }
+
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
+            {
+                if (!string.IsNullOrEmpty(this.CertificateRegistryEntry)
+                    && !string.IsNullOrEmpty(this.CertificateKeyRegistryEntry)
+                    )
+                {
+                    string ssl_cert = null;
+                    string ssl_key = null;
+
+                    using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser
+      .OpenSubKey(@"Software\COR\All", false))
+                    {
+                        ssl_cert = (string)key.GetValue("ssl_cert");
+                        ssl_key = (string)key.GetValue("ssl_key");
+                        key.Close();
+                    } // End Using key 
+
+                    System.ReadOnlySpan<char> certa = System.MemoryExtensions.AsSpan(ssl_cert);
+                    System.ReadOnlySpan<char> keya = System.MemoryExtensions.AsSpan(ssl_key);
+                    return System.Security.Cryptography.X509Certificates.X509Certificate2
+                        .CreateFromPem(certa, keya);
+                } // End if CertEntriesExist 
+
+            } // End if IsWindows 
+            
+            throw new System.IO.InvalidDataException("Need SSL-Key");
+        } // End Function GetCertificate 
+
+
+    } // End HttpsEndpoint  
+
+
     public class EndpointPOCO
     {
-        public string[] Http { get; set; }
-        public string[] Https { get; set; }
+        public System.Collections.Generic.List<string> Http { get; set; }
+        public System.Collections.Generic.List<HttpsEndpoint> Https { get; set; }
+
+
+        public EndpointPOCO()
+        {
+            this.Http = new System.Collections.Generic.List<string>();
+            this.Https = new System.Collections.Generic.List<HttpsEndpoint>();
+        }
+
     }
+
 
     public class POCO
     {
         public EndpointPOCO Endpoints { get; set; }
-        
+
+        public POCO()
+        {
+            this.Endpoints = new EndpointPOCO();
+        }
+
+
     }
 
     // 1. add  <AspNetCoreHostingModel>InProcess</AspNetCoreHostingModel> to project file 
@@ -58,6 +105,29 @@ namespace TestApplicationHttps
         public static void Main(string[] args)
         {
             // https://medium.com/@mvuksano/how-to-properly-configure-your-nginx-for-tls-564651438fe0
+
+            var poco = new POCO();
+            poco.Endpoints.Http.AddRange(new string[] { "http://*:5000", "http://localhost:5000", "http://machinename:5000" });
+
+            poco.Endpoints.Https.Add(new HttpsEndpoint()
+            {
+                Url = "https://*:5005",
+                CertificateRegistryEntry = "ssl_cert",
+                CertificateKeyRegistryEntry = "ssl_key"
+            });
+
+
+            poco.Endpoints.Https.Add(new HttpsEndpoint()
+            {
+                Url = "https://*:5006",
+                CertificateRegistryEntry = "ssl_cert",
+                CertificateKeyRegistryEntry = "ssl_key"
+            });
+
+            string json = System.Text.Json.JsonSerializer.Serialize(poco);
+            System.Console.WriteLine(json);
+
+
 
             // ln -s /etc/nginx/sites-available/example.int example.int
             // cat cert.pem ca.pem > fullchain.pem
@@ -141,6 +211,20 @@ namespace TestApplicationHttps
                                     .HttpsDefaults); // End ConfigureHttpsDefaults 
 
                                 // serverOptions.Listen(System.Net.IPAddress.Loopback, 5001,
+
+
+                                // Bind directly to a socket handle or Unix socket
+                                // serverOptions.ListenHandle(123456);
+                                // serverOptions.ListenUnixSocket("/tmp/kestrel-test.sock");
+
+
+                                // serverOptions.Listen(System.Net.IPAddress.Loopback, port: 5002);
+                                // serverOptions.ListenAnyIP(5003);
+                                // serverOptions.ListenLocalhost(5004, opts => opts.UseHttps());
+                                // serverOptions.ListenLocalhost(5005, opts => opts.UseHttps());
+
+
+
                                 serverOptions.ListenAnyIP(5005,
                                     delegate(Microsoft.AspNetCore.Server.Kestrel.Core.ListenOptions listenOptions)
                                     {
@@ -185,6 +269,11 @@ namespace TestApplicationHttps
                             // .UseApplicationInsights()
                             ;
                     }); // End ConfigureWebHostDefaults 
+
         } // End Function CreateHostBuilder 
+
+
     } // End Class Program 
+
+
 } // End Namespace TestApplicationHttps 
